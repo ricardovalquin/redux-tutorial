df0d5c0e1f8cc83aef895e82a89fd085d3a1a8dd
Ricardo Valencia Quintero <ricardovalquin@gmail.com>
2017-07-28 08:45:08 -0500

    Created the reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'JS', pages: 101 },
+    { title: 'Harry Potter', pages: 300 },
+    { title: 'the dark tower', pages: 234 },
+    { title: 'Eloquent Ruby', pages: 1 },
+  ];
+}
+
